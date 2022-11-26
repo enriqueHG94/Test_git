3e883b7c881bfe3191929bed177efb3c8d52dbd5
Enrique Hervás Guerrero <enrique_hervas_94@hotmail.com>
2022-11-26 21:20:27 +0100

    MyOperations

---
+++ Methods.py
@@ -0,0 +1,9 @@
+class MyOperations:
+    def sum(a,b):
+        result = a+b
+        return result
+    def subtract(c,d):
+        result = c-d
+        return result
+print(MyOperations.sum(27,17))
+print(MyOperations.subtract(27,17))
